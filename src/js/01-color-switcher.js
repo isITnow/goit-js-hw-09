5adbd88988f9a2d7fde0168b69931adfb5b842bb
Roman Serediuk <box4rom@gmail.com>
2022-08-10 23:36:47 +0300

    01-task completed

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+let timerId = null;
+stopBtn.setAttribute('disabled', 'disabled');
+
+startBtn.addEventListener('click', onStartBtnClick);
+stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick() {
+    timerId = setInterval(setBodyColor, 1000);
+    function setBodyColor() {
+        const color = getRandomHexColor();
+        document.body.style.backgroundColor = `${color}`;
+        startBtn.setAttribute('disabled', 'disabled');
+        stopBtn.removeAttribute('disabled');
+    }
+}
+
+function onStopBtnClick() {
+    clearInterval(timerId);
+    startBtn.removeAttribute('disabled');
+    stopBtn.setAttribute('disabled', 'disabled');
+}
